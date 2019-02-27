f6019173ddbb33a77675f6cd127d0d25fa1dcbc0
Hippolyte Glaus <hippolyte.glaus@gmail.com>
2019-02-26 20:49:47 -0500

    Add docker-compose
    - Format python file

--- db/model.py
+++ db/model.py
@@ -7,9 +7,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
- 
+
+
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "user"
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
@@ -19,11 +20,16 @@ class User(Base):
     password = Column(String(250), nullable=False)
 
     def to_dict(self):
-    	return {"user_id": self.id, "first_name": self.first_name, "last_name": self.last_name, "email": self.email}
+        return {
+            "user_id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+        }
 
 
-engine = create_engine('postgresql://postgres:feedbuzz@localhost/feedbuzz')
- 
+engine = create_engine("postgresql://postgres:feedbuzz@localhost:5430/feedbuzz")
+
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
 Base.metadata.create_all(engine)
