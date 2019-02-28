5e5355737e62c63973012cdb9c55743de400f753
Louis Contant <louis.contant.1@gmail.com>
2019-02-28 18:56:09 -0500

    started adding product endpoint

--- db/dao.py
+++ db/dao.py
@@ -1,7 +1,8 @@
-from .model import User, Base
+from .model import User, Base, Product
 from abc import ABC, abstractmethod
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 
 class BaseDAO(ABC):
 
@@ -13,21 +14,38 @@ class BaseDAO(ABC):
 
     @abstractmethod
     def get(self, id):
-    	pass
+        pass
+
 
 class UserDAO(BaseDAO):
 
-	def get(self, id):
-		Session = sessionmaker(bind=self._engine)
-		session = Session()
-		user = session.query(User).filter_by(id=id).first()
-		session.close()
-		return user
+    def get(self, id):
+        Session = sessionmaker(bind=self._engine)
+        session = Session()
+        user = session.query(User).filter_by(id=id).first()
+        session.close()
+        return user
 
-	def get_by_username(self, username):
-		Session = sessionmaker(bind=self._engine)
-		session = Session()
-		user = session.query(User).filter_by(email=username).first()
-		session.close()
-		return user
-    
+    def get_by_username(self, username):
+        Session = sessionmaker(bind=self._engine)
+        session = Session()
+        user = session.query(User).filter_by(email=username).first()
+        session.close()
+        return user
+
+
+class ProductDAO(BaseDAO):
+
+    def get(self, id):
+        Session = sessionmaker(bind=self._engine)
+        session = Session()
+        product = session.query(Product).filter_by(id=id).first()
+        session.close()
+        return product
+
+    def get_by_name(self, name):
+        Session = sessionmaker(bind=self._engine)
+        session = Session()
+        product = session.query(Product).filter_by(name=name).first()
+        session.close()
+        return product
