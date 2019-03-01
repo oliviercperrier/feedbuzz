73e2197c7bae5dac279fa6682dd264ef95717c4e
Olivier Castro-Perrier <olivier.castro-perrier@bonlook.com>
2019-03-01 08:20:54 -0500

    add comment

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ if app.config.ENV == "production":
     app.static('/', './client/build')
     app.static('/static', './client/static')
 
+"""
+    Ne fonctionne pass calisse...
+    Avec React, les routes sont client side. Donc si je navigate dans 
+    l'application normalement tout est chill. Par contre, si j'essais d'accéder
+    directement à une page en ecrivant dans le browser ex.: http://localhost/authenticate
+    j'ai une page not found.
+
+    La route suivante est sensé rediriger l'app vers index.html quand un endpoint n'existe
+    pas coté backend pour que ce soit le frontend qui le gère, mais ca fonctionne pas..
+    C'est pourtant comme ca qu'il faut faire.
+"""
 @app.route("/")
 async def index(request):
     return await file('./client/build/index.html')
