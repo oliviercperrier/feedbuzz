ab196b41e356d8f7e2f7b339350cef58b8b032db
Olivier Castro-Perrier <olivier.castro-perrier@bonlook.com>
2019-03-07 01:55:51 -0500

    cleaned build

---
+++ client/build/precache-manifest.181db0b0f6fac1bad46ce66b44f9a84b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eeeab213edd0673416a0",
+    "url": "/static/js/main.eeeab213.chunk.js"
+  },
+  {
+    "revision": "3a11079f60cd1d61d840",
+    "url": "/static/js/2.3a11079f.chunk.js"
+  },
+  {
+    "revision": "8eda6f5486d4672bbbe3adad3d9c4e49",
+    "url": "/index.html"
+  }
+];
