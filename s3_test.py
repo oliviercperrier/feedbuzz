e36d2e8f33f6918daff0c6cf4c35a5c894864795
Louis Contant <louis.contant.1@gmail.com>
2019-03-14 19:38:19 -0400

    Made a functionning request on the bucket

--- s3_test.py
+++ s3_test.py
@@ -1,13 +1,17 @@
 import boto3
 import base64
-s3 = boto3.client(
+s3 = boto3.resource(
     's3',
     aws_access_key_id='AKIAJPL6XE3LHXEDIEKA',
     aws_secret_access_key='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'
 )
 
-response = s3.get_bucket_acl(
-    Bucket='feedbuzz'
+response = s3.Bucket(
+    'feedbuzz'
 )
+
+
+for object in response.objects.all():
+    print(object)
 
 print(response)
