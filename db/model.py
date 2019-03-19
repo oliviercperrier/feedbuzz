e20d6c8fbb69eb21e2eae6ff1f83c844804a6d8f
Hippolyte Glaus <hippolyte.glaus@gmail.com>
2019-03-19 19:36:29 -0400

    wip comment

--- db/model.py
+++ db/model.py
@@ -25,7 +25,7 @@ class Product(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(250), nullable=False, unique=True)
     type_id = Column(Integer, ForeignKey("product_type.id"))
-    #type = relationship("ProductType", backref="product")
+    # type = relationship("ProductType", backref="product")
     url = Column(String(256), nullable=False, unique=False)
     image_url = Column(String(256), nullable=False, unique=False)
     thc_min = Column(Float)
@@ -35,16 +35,17 @@ class Product(Base):
     prices = relationship("ProductPrice", back_populates="product")
 
     def to_dict(self):
-        return {"id": self.id
-            , "name": self.name
-            , "type_id": self.type_id
-            , "url": self.url
-            , "image_url": self.image_url
-            , "thc_min": self.thc_min
-            , "thc_max": self.thc_max
-            , "cbd_min": self.cbd_min
-            , "cbd_max": self.cbd_max
-            }
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type_id": self.type_id,
+            "url": self.url,
+            "image_url": self.image_url,
+            "thc_min": self.thc_min,
+            "thc_max": self.thc_max,
+            "cbd_min": self.cbd_min,
+            "cbd_max": self.cbd_max,
+        }
 
 
 class ProductType(Base):
@@ -72,20 +73,47 @@ class RefreshToken(Base):
     user_id = Column(Integer, nullable=False)
     token = Column(String(500), nullable=False)
 
+
+class CommentRatingStep(Base):
+    __tablename__ = "comments_rating_step"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String(100))
+    common = Column(ARRAY(String(50)))
+    added = Column(ARRAY(String(50)))
+    rating = Column(Integer)
+    comment_id = Column(Integer, ForeignKey("comments.id"), backref="comments")
+
+    def to_dict(self):
+        return {
+                "id": self.id,
+                "type": self.type,
+                "common": self.common,
+                "added": self.added,
+                "rating": self.rating,
+                }
+
+
 class Comment(Base):
-    __tablename__= "comments"
+    __tablename__ = "comments"
     id = Column(Integer, primary_key=True, autoincrement=True)
     author_id = Column(Integer, ForeignKey("user.id"))
     parent_comment_id = Column(Integer, ForeignKey("comment.id"))
     content = Column(String(2500), nullable=False)
     score = Column(Integer, nullable=False, default=0)
     replies = relationship("Comment")
+    comment_rating_step = relationship("CommentRatingStep")
 
     def to_dict(self):
-        return {"id": self.id, "author_id": self.author_id, "content": self.content, "score": self.score, "replies": [reply.to_dict() for reply in self.replies]}
+        return {
+            "id": self.id,
+            "author_id": self.author_id,
+            "content": self.content,
+            "score": self.score,
+            "replies": [reply.to_dict() for reply in self.replies],
+        }
 
 
-#engine = create_engine("postgresql://postgres:feedbuzz@localhost:5430/feedbuzz")
+# engine = create_engine("postgresql://postgres:feedbuzz@localhost:5430/feedbuzz")
 
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
